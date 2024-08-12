cdb686b491fbd27399e25d0bcc24022c07d447a8
W.L.M <mrbomrwu@gmail.com>
2024-08-12 14:54:31 +0800

    update

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.font_manager as fm
+
+# 设置中文字体
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体字体
+plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 
 df = pd.read_excel(
     'C:/Users/海棠微雨/Desktop/C题/附件1.xlsx',
@@ -7,28 +12,40 @@ df = pd.read_excel(
     header=0,  # 第一行作为列名
     usecols='A:D',  # 读取 A 到 D 列
     index_col=None,  # 第一列作为行索引
-    skiprows=0,  # 跳过第一行
-    nrows=None  # 读取前 10 行
+    skiprows=0,  # 不跳过
+    nrows=None  # 读取所有 行
 )
-print(df)
-print(type(df))
-print(df['分类'].head())
-# # 数据
-# labels = ['花叶类', '花菜类', '辣椒类', '茄类', '食用菌', '水生根茎类']
-# sizes = [A, B, C, D, E, F]
-# colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-# explode = (0.1, 0.05, 0.1, 0.05)  # 仅"爆炸"第一个扇区
+data = df['分类名称']
+A, B, C, D, E, F = 0, 0, 0, 0, 0, 0
+for i in data:
+    if i == '花叶类':
+        A += 1
+    elif i == '花菜类':
+        B += 1
+    elif i == '辣椒类':
+        C += 1
+    elif i == '茄类':
+        D += 1
+    elif i == '食用菌':
+        E += 1
+    elif i == '水生根茎类':
+        F += 1
+# 数据
+labels = ['花叶类', '花菜类', '辣椒类', '茄类', '食用菌', '水生根茎类']
+sizes = [A, B, C, D, E, F]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0.05, 0.1, 0.05, 0.05, 0.05)  # 仅"爆炸"第一个扇区
 
-# # 绘制饼状图
-# plt.figure(figsize=(8, 8))
-# plt.pie(sizes, labels=labels, colors=colors, explode=explode, autopct='%1.1f%%', startangle=140, shadow=True)
+# 绘制饼状图
+plt.figure(figsize=(8, 8))
+plt.pie(sizes, labels=labels, colors=colors, explode=explode, autopct='%1.1f%%', startangle=140, shadow=True)
 
-# # 设置标题
-# plt.title('Pie Chart with Exploded Slice')
+# 设置标题
+plt.title('Pie Chart with Exploded Slice')
 
-# # 添加图例
-# plt.legend(labels, loc="best")
+# 添加图例
+plt.legend(labels, loc="best")
 
-# # 显示图形
-# plt.show()
+# 显示图形
+plt.show()
 

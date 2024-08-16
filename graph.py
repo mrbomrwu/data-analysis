1ba3ba87bdf6a72d8e0e457ae33866d2a8ba7386
W.L.M <mrbomrwu@gmail.com>
2024-08-16 17:10:05 +0800

    update

--- graph.py
+++ graph.py
@@ -1,57 +1,22 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import matplotlib.font_manager as fm
+from scipy.interpolate import interp1d
 
-# 设置中文字体
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体字体
-plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
+# 给定的 y 值
+y = [1, 2, 5, 8, 9, 7]
+x = np.arange(len(y))  # x 轴的索引值
 
-df = pd.read_excel(
-    'C:/Users/海棠微雨/Desktop/C题/附件1.xlsx',
-    sheet_name='Sheet1',
-    header=0,  # 第一行作为列名
-    usecols='A:D',  # 读取 A 到 D 列
-    index_col=None,  # 第一列作为行索引
-    skiprows=0,  # 不跳过
-    nrows=None  # 读取所有 行
-)
+# 创建插值函数
+f = interp1d(x, y, kind='cubic')
 
-df1 = pd.read_excel(
-    'C:/Users/海棠微雨/Desktop/C题/附件2.xlsx',
-    sheet_name='Sheet1',
-    header=0,  # 第一行作为列名
-    usecols='A:G',  # 读取 A 到 D 列
-    index_col=None,  # 第一列作为行索引
-    skiprows=0,  # 不跳过
-    nrows=None  # 读取所有 行
-)
+# 生成更多的 x 值
+x_smooth = np.linspace(0, len(y)-1, 500)
+y_smooth = f(x_smooth)
 
-encoder = df['单品编码']
-name = df['分类名称']
-
-# 使用 zip() 函数将两个列表合并为一个字典
-my_dict = dict(zip(encoder, name))
-
-encoder1 = df1['单品编码']
-date = df1['扫码销售时间']
-Sales_status = df1['销售类型']
-
-my_dict1 = dict(zip(date, encoder1))
-first_key, first_value = next(iter(my_dict1.items()))
-print(first_key, first_value)
-
-# # 绘制多线图
-# plt.figure(figsize=(10, 6))
-# for column in df.columns[1:]:
-#     plt.plot(df['时间'], df[column], label=column)
-
-# # 添加图例
-# plt.legend(loc='upper left')
-
-# # 添加标题和标签
-# plt.title('多线图示例')
-# plt.xlabel('时间')
-# plt.ylabel('数值')
-
-# # 显示图形
-# plt.show()
+# 绘制曲线
+plt.plot(x_smooth, y_smooth)
+plt.scatter(x, y, color='red')  # 标出原始点
+plt.title("Smooth Curve")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.show()
